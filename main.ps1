67eff3163f63d434466a0273870ffcb71c02b393
Radoslaw Nawrocki <radziomenek@gmail.com>
2021-01-10 17:47:30 +0100

    added icon, translated env: variables to English, small visual edits in main script.

--- main.ps1
+++ main.ps1
@@ -29,7 +29,7 @@ $breaks = $main_form.Height / 20
 $drawpoint = 20
 $movesize = 20
 
-$P = Import-Csv -Path .\Enviromentals_desc.csv 
+$P = Import-Csv -Path .\Environmentals_desc.csv 
 
 #Copy to clipboard button Function
 function OnClick($Sender, $EventArgs){     
@@ -41,24 +41,26 @@ function OnClick($Sender, $EventArgs){
     }
     if($Sender.varname -notlike $null)
     {
-        write-host "elo, rozpoczynam sie"
         $lineToCopy = $Sender.varname
-        write-host "wybieram z tabeli..."
         $textToCopy = $P | where-Object func -like $lineToCopy | select-Object -ExpandProperty desc
         $helpfunc = New-object System.Windows.Forms.Form
-        $helpfunc.Text = "Hilfe, adolf-kun <3"
+        $helpfunc.Text = "What is this?"
         $helpfunc.Width = 500
-        $helpfunc.Height = 500
+        $helpfunc.Height = 200
         $helpfunc.AutoScale = $true  
-        $helpname = new-object System.Windows.Forms.TextBox
-        $helpname.height = 50
-        $helpname.Width = 60
-        $helpname.Location = New-Object System.Drawing.Point(200, 100)
+        $helpname = new-object System.Windows.Forms.RichTextBox
+        $helpname.height = 30
+        $helpname.Width = 400
+        $helpname.Location = New-Object System.Drawing.Point(10, 10)
+        $oldFont = $helpname.Font
+        $font = New-Object Drawing.Font($oldFont.FontFamily, $oldFont.Size, [Drawing.FontStyle]::Bold)
+        $helpname.Font = $font
+        $helpname.ReadOnly = $true
         $helpname.Text = "$lineToCopy"
-        $helpdesc = new-object System.Windows.Forms.TextBox
-        $helpdesc.height = 50
+        $helpdesc = new-object System.Windows.Forms.Label
+        $helpdesc.height = 400
         $helpdesc.Width = 400
-        $helpdesc.Location = New-Object System.Drawing.Point(50, 200)
+        $helpdesc.Location = New-Object System.Drawing.Point(10, 40)
         $helpdesc.Text = "$textToCopy"
         $helpfunc.Controls.Add($helpname)
         $helpfunc.Controls.Add($helpdesc)
@@ -79,7 +81,7 @@ for ($i = 1; $i -ne ($breaks - 3); $i = $i + 1)
     $(get-variable -Name "field$i").Value = new-object System.Windows.Forms.TextBox
     $(get-variable -Name "field$i").Value.Height = 30
     $(get-variable -Name "field$i").Value.Width = 700
-    $(get-variable -Name "field$i").Value.Location = New-Object System.Drawing.Point(370, $drawpoint)
+    $(get-variable -Name "field$i").Value.Location = New-Object System.Drawing.Point(250, $drawpoint)
     $(get-variable -Name "field$i").Value.ReadOnly = $true
     $(get-variable -Name "field$i").Value.Text = "$valueWriteout"
     $(get-variable -Name "label$i").Value = new-object System.Windows.Forms.Label
@@ -90,14 +92,14 @@ for ($i = 1; $i -ne ($breaks - 3); $i = $i + 1)
     $(get-variable -Name "question$i").Value = new-object System.Windows.Forms.Button
     $(get-variable -Name "question$i").Value.Height = 20
     $(get-variable -Name "question$i").Value.Width = 20
-    $(get-variable -Name "question$i").Value.Location = New-Object System.Drawing.Point(1080, $drawpoint)
+    $(get-variable -Name "question$i").Value.Location = New-Object System.Drawing.Point(950, $drawpoint)
     $(get-variable -Name "question$i").Value.Text = "?"
     $(get-variable -Name "question$i").Value | Add-Member -NotePropertyName varname -NotePropertyValue $nameWriteout
     $(get-variable -Name "question$i").Value.add_click({OnClick $this $_})
     $(get-variable -Name "copier$i").Value = new-object System.Windows.Forms.Button
     $(get-variable -Name "copier$i").Value.Height = 20
     $(get-variable -Name "copier$i").Value.Width = 100
-    $(get-variable -Name "copier$i").Value.Location = New-Object System.Drawing.Point(1102, $drawpoint)
+    $(get-variable -Name "copier$i").Value.Location = New-Object System.Drawing.Point(970, $drawpoint)
     $(get-variable -Name "copier$i").Value.Text = "copy to clipboard $i"
     $(get-variable -Name "copier$i").Value | Add-Member -NotePropertyName Number -NotePropertyValue $i
     $(get-variable -Name "copier$i").Value.add_click({OnClick $this $_})
