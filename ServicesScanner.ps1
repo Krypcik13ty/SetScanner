8eafd59c94c39682f9432520d76d787f8e7e7540
Radoslaw Nawrocki <radziomenek@gmail.com>
2021-01-13 22:41:20 +0100

    Fixed resoluton bug leaving decimals at the end of $breaks, added function buttons to ServicesScanner, small adjustments to descriptions, added registry edit file to enable executionpolicy (currently confirmed working on win8.1 & 10).

--- ServicesScanner.ps1
+++ ServicesScanner.ps1
@@ -41,25 +41,42 @@ $form.Controls.Add($svcdetails)
 $listview1_ItemSelectionChanged=[System.Windows.Forms.ListViewItemSelectionChangedEventHandler]{
     #Event Argument: $_ = [System.Windows.Forms.ListViewItemSelectionChangedEventArgs]
     if($($_.IsSelected) -like $true)
-    {
+    {   
+        $import = get-service
         $svcclicked = $($_.Item.Text)
         $svcname.Text = $($svcclicked)
         $svcstatus.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty Status
         $svclongname.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty DisplayName
         $svcdetails.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty Description
         
+        if($svcstatus.Text -notlike "Running")
+        {
+            $resetButton.Enabled = $False
+        }
+        else
+        {
+            $resetButton.Enabled = $True
+        }
+        if($svcstatus.Text -like "Running")
+        {
+            $powerButton.Text = "Disable Service"
+        }
+        elseif ($svcstatus.Text -like "Stopped") 
+        {      
+            $powerButton.Text = "Enable Service"
+        }
     }
 }
 $listBox.Add_ItemSelectionChanged($listview1_ItemSelectionChanged)
 
-$import = get-service
+
 $image1 = [System.Drawing.Image]::Fromfile('.\gear_on256.png')
 $image2 = [System.Drawing.Image]::Fromfile('.\gear_off256.png')
 $imageList = New-Object System.Windows.Forms.ImageList
 $ImageList.Images.Add($image1)
 $ImageList.Images.Add($image2)
 $listBox.LargeImageList = $imageList
-
+$import = get-service
 ForEach($array in $import){
     $item = New-Object System.Windows.Forms.ListviewItem($array.Name)
     
@@ -77,6 +94,122 @@ $listBox.Height = 780
 $form.Controls.Add($listBox)
 $form.Topmost = $true
 
+$resetButton = new-object System.Windows.Forms.Button
+$resetButton.Text = "Restart Service"
+$resetButton.Width = 128
+$resetButton.height = 50
+$resetButton.Location =  new-Object System.Drawing.Point(608, 450)
+$resetButton.Add_Click(
+    {
+        $powerButton.Enabled = $False
+        $selectedservice = $svcname.Text
+        $powerButton.Text = "please wait"
+        $import = get-service
+        $form.Enabled = $false
+        $ErrorActionPreference = "Stop"
+        try
+        {
+            Restart-Service -Name $selectedservice
+        }
+        catch 
+        {          
+            Start-Process -FilePath "powershell" -Verb RunAs -ArgumentList "-command Restart-Service -Name $selectedservice"     
+        }
+        $import = get-service
+        $svcclicked = $($_.Item.Text)
+        $svcname.Text = $($svcclicked)
+        $svcstatus.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty Status
+        $svclongname.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty DisplayName
+        $svcdetails.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty Description
+        if($svcstatus.Text -notlike "Running")
+        {
+            $resetButton.Enabled = $False
+        }
+        else
+        {
+            $resetButton.Enabled = $True
+        }
+        $form.Enabled = $true
+        $ErrorActionPreference = "SilentlyContinue"
+}
+)
+$powerButton = new-object System.Windows.Forms.Button
+$powerButton.Width = 128
+$powerButton.height = 50
+$powerButton.Location =  new-Object System.Drawing.Point(480, 450)
+$powerButton.Add_Click(
+    {
+            $powerButton.Enabled = $False
+            $selectedservice = $svcname.Text
+            $powerButton.Text = "please wait"
+            $import = get-service
+            $form.Enabled = $false
+            $ErrorActionPreference = "Stop"
+            try
+            {
+                
+                if(($import | where-object Name -like "$selectedservice" | select-object -ExpandProperty Status) -like "Running")
+                {
+                    Stop-Service -Name $selectedservice
+                    $($listbox.Items | Where-object Text -Like $selectedservice).ImageIndex = 1
+                }
+                else
+                {
+                    Start-Service -Name $selectedservice
+                    $($listbox.Items | Where-object Text -Like $selectedservice).ImageIndex = 0
+                }                 
+                   
+                $powerButton.Text = "Service Stopped!"
+                start-sleep -Seconds 2 
+                $powerButton.Text = "Enable Service"
+            }
+            catch 
+            {
+                
+                $powerButton.Text = "elevating..."
+                if(($import | where-object Name -like "$selectedservice" | select-object -ExpandProperty Status) -like "Running")
+                {
+                    Start-Process -FilePath "powershell" -Verb RunAs -ArgumentList "-command Stop-Service -Name $selectedservice"
+                    $($listbox.Items | Where-object Text -Like $selectedservice).ImageIndex = 1
+                    $powerButton.Text = "Service Stopped!"
+                    start-sleep -Seconds 2 
+                    $powerButton.Text = "Enable Service"
+                }
+                else
+                {
+                    Start-Process -FilePath "powershell" -Verb RunAs -ArgumentList "-command Start-Service -Name $selectedservice"
+                    $($listbox.Items | Where-object Text -Like $selectedservice).ImageIndex = 0
+                    $powerButton.Text = "Service Stopped!"
+                    start-sleep -Seconds 2 
+                    $powerButton.Text = "Enable Service"
+                }                               
+                
+                Set-variable -Name $listBox.Items | where-Object Text -like $selectedservice | Select-Object ImageIndex
+              
+                
+                
+            }
+            $import = get-service
+            $svcclicked = $($_.Item.Text)
+            $svcname.Text = $($svcclicked)
+            $svcstatus.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty Status
+            $svclongname.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty DisplayName
+            $svcdetails.Text = $import | where-object Name -like "$($svcclicked)" | select-object -ExpandProperty Description
+            $form.Enabled = $true
+            $powerButton.Enabled = $true 
+            if($svcstatus.Text -notlike "Running")
+            {
+                $resetButton.Enabled = $False
+            }
+            else
+            {
+                $resetButton.Enabled = $True
+            }
+            $ErrorActionPreference = "SilentlyContinue"
+    }
+)
+$form.Controls.Add($resetButton)
+$form.Controls.Add($powerButton)
 
 
 
